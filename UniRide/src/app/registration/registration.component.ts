107a2e1b625773a90c27f842dcdcc292497f1e77
TheFanaticV2 <chefyaya93@outlook.com>
2023-11-05 16:01:37 +0100

    patch : request post ok

--- UniRide/src/app/registration/registration.component.ts
+++ UniRide/src/app/registration/registration.component.ts
@@ -21,17 +21,25 @@ export class RegistrationComponent {
       gender: ['', Validators.required],
       phone_number: ['', Validators.required],
       description: ['', Validators.required],
-    }, {
     });
   }
 
-
   onSubmit() {
-      if (this.inscriptionForm.valid) {
-      const formData = this.inscriptionForm.value;
-      console.log(formData);
-
+    if (this.inscriptionForm && this.inscriptionForm.valid) {
+      const formData = new FormData();
+      formData.append('login', this.inscriptionForm.get('login')?.value || '');
+      formData.append('firstname', this.inscriptionForm.get('firstname')?.value || '');
+      formData.append('lastname', this.inscriptionForm.get('lastname')?.value || '');
+      formData.append('student_email', this.inscriptionForm.get('student_email')?.value || '');
+      formData.append('password', this.inscriptionForm.get('password')?.value || '');
+      formData.append('password_confirmation', this.inscriptionForm.get('password_confirmation')?.value || '');
+      formData.append('gender', this.inscriptionForm.get('gender')?.value || '');
+      formData.append('phone_number', this.inscriptionForm.get('phone_number')?.value || '');
+      formData.append('description', this.inscriptionForm.get('description')?.value || '');
+  
       const url = 'https://127.0.0.1:5050/user/register'; // Assurez-vous que l'URL correspond à votre route Flask
+  
+      // Envoyer la requête POST avec FormData
       this.http.post(url, formData).subscribe(
         (response) => {
           console.log(response);
@@ -42,4 +50,5 @@ export class RegistrationComponent {
       );
     }
   }
+  
 }
