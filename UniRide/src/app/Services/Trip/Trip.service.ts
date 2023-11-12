cb8443eae75b61233a2323f10e16a03989accb6e
Ayoub Bouaziz <aybouaziz@iut.univ-paris8.fr>
2023-11-12 14:14:24 +0100

    add: connect to API

--- UniRide/src/app/Services/Trip/Trip.service.ts
+++ UniRide/src/app/Services/Trip/Trip.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -10,12 +12,26 @@ export class TripService {
 
   constructor(private http: HttpClient) { }
 
-  createTrip(tripData: any) {
-    return this.http.post(this.apiUrl + '/trip/propose', tripData);
-  }
-  createAddress(tripaddressData: any) {
-    
-    return this.http.post(this.apiUrl + '/address/add', tripaddressData);
+ 
+  createTrip(tripData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}trip/propose`, tripData).pipe(
+      map((response: any) => {
+        // Utilisez les données de la réponse comme nécessaire
+        const tripId = response.trip_id;
+        console.log('ID du trajet créé :', tripId);
+        return tripId;
+      })
+    );
   }
 
+  createAddress(tripaddressData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}address/add`, tripaddressData).pipe(
+      map((response: any) => {
+        // Utilisez les données de la réponse comme nécessaire
+        const idAddress = response.id_address;
+        console.log('ID de l\'adresse créée :', idAddress);
+        return idAddress;
+      })
+    );
+  }
 }
--- UniRide/src/app/create-trip/create-trip.component.ts
+++ UniRide/src/app/create-trip/create-trip.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TripService } from '../Services/Trip/Trip.service'; 
+import { TripService } from '../Services/Trip/Trip.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,14 +7,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './create-trip.component.html',
   styleUrls: ['./create-trip.component.css']
 })
-export class CreateTripComponent implements OnInit{
-  createTripForm: FormGroup = new FormGroup({});
+export class CreateTripComponent implements OnInit {
+  createTripForm!: FormGroup;
 
-
-    constructor(
-      private formBuilder: FormBuilder,
-      private tripService: TripService
-      ) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private tripService: TripService
+  ) {}
 
   ngOnInit(): void {
     this.createTripForm = this.formBuilder.group({
@@ -28,10 +27,66 @@ export class CreateTripComponent implements OnInit{
 
   onSubmit() {
     if (this.createTripForm.valid) {
-      // Handle form submission
-      console.log('Form submitted successfully', this.createTripForm.value);
+      const tripData = {
+        adresseDepart: this.createTripForm.value.adresseDepart,
+        adresseArrivee: this.createTripForm.value.adresseArrivee,
+        date: this.createTripForm.value.date,
+        horaire: this.createTripForm.value.horaire,
+        nombrePassagers: this.createTripForm.value.nombrePassagers
+      };
+      console.log('Données du formulaire :', tripData);
+      
+      // Appeler le service pour créer l'adresse de départ
+      this.tripService.createAddress({ address: tripData.adresseDepart }).subscribe(
+        (departAddressResponse) => {
+          // Adapter cette partie en fonction de la structure réelle de la réponse
+          const departAddressId = this.extractIdFromResponse(departAddressResponse);
+  
+          // Appeler le service pour créer l'adresse d'arrivée
+          this.tripService.createAddress({ address: tripData.adresseArrivee }).subscribe(
+            (arriveeAddressResponse) => {
+              // Adapter cette partie en fonction de la structure réelle de la réponse
+              const arriveeAddressId = this.extractIdFromResponse(arriveeAddressResponse);
+  
+              // Maintenant, vous avez les IDs des adresses, vous pouvez les utiliser pour créer le trajet
+              const tripWithAddresses = {
+                adresseDepartId: departAddressId,
+                adresseArriveeId: arriveeAddressId,
+                date_horaire: tripData.date + ' ' + tripData.horaire + ":00",
+                nombrePassagers: tripData.nombrePassagers
+              };
+  
+              // Appeler le service pour créer le trajet
+              this.tripService.createTrip(tripWithAddresses).subscribe(
+                (tripId) => {
+                  // Gérer la réponse du service, par exemple, afficher un message de succès
+                  console.log('Trajet créé avec succès, ID :', tripId);
+                },
+                (tripError) => {
+                  // Gérer les erreurs lors de la création du trajet
+                  console.error('Erreur lors de la création du trajet', tripError);
+                }
+              );
+            },
+            (arriveeAddressError) => {
+              // Gérer les erreurs lors de la création de l'adresse d'arrivée
+              console.error('Erreur lors de la création de l\'adresse d\'arrivée', arriveeAddressError);
+            }
+          );
+        },
+        (departAddressError) => {
+          // Gérer les erreurs lors de la création de l'adresse de départ
+          console.error('Erreur lors de la création de l\'adresse de départ', departAddressError);
+        }
+      );
     } else {
-      console.error('Form is invalid');
+      console.error('Le formulaire est invalide');
     }
+  }
+
+  // Fonction générique pour extraire l'identifiant de la réponse
+  private extractIdFromResponse(response: any): any {
+    // Adapter cette partie en fonction de la structure réelle de la réponse
+    return response.id_address || response.data?.id_address || null;
   }
 }
