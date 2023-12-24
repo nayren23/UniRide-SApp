45c58baf29ced1f7ef0799bba8743be81173e26f
Nayren <rchouchane@iut.univ-paris8.fr>
2023-12-24 13:01:13 +0100

    fix: we can now search by student in the table

--- UniRide/src/app/models/document-verification-display.ts
+++ UniRide/src/app/models/document-verification-display.ts
@@ -1,13 +1,13 @@
-import { Person } from "./person";
+import { Student } from "./student";
 
 export class DocumentVerificationDisplay {
     request_number!: number;
     documents_to_verify!: number;
-    person!: Person;
+    student!: Student;
 
-    constructor(request_number: number, documents_to_verify: number, person: Person) {
+    constructor(request_number: number, documents_to_verify: number, student: Student) {
         this.request_number = request_number;
         this.documents_to_verify = documents_to_verify;
-        this.person = person;
+        this.student = student;
     }
 }
--- UniRide/src/app/models/student.ts
+++ UniRide/src/app/models/student.ts
@@ -1,10 +1,12 @@
-export class Etudiant {
+export class Student {
 
     full_name!: String;
     profile_picture?: String
+    id?: number;
 
-    constructor(full_name: String, profile_picture?: String) {
+    constructor(full_name: String, profile_picture?: String, id?: number) {
         this.full_name = full_name;
         this.profile_picture = profile_picture;
+        this.id = id;
     }
 }
