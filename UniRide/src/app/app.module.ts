46339226d60b7f96e298440fea1c84f83a3c2215
Steven Yang <steven.yang.ys@gmail.com>
2024-01-17 21:49:27 +0100

    feat: change forgot password

--- UniRide/src/app/app.module.ts
+++ UniRide/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
