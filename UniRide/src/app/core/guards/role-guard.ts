65aa9baf933bab035c57152c309ab2eb0474d9af
Steven Yang <steven.yang.ys@gmail.com>
2024-01-17 16:08:39 +0100

    feat: add validate-passenger route

--- UniRide/src/app/core/guards/role-guard.ts
+++ UniRide/src/app/core/guards/role-guard.ts
@@ -6,12 +6,12 @@ export function roleGuardFactory(route: ActivatedRouteSnapshot, state: RouterSta
   const authService = inject(AuthService);
   const router = inject(Router);
   const requiredRoles = route.data['roles'] as number[];
-    if (authService.hasRole(requiredRoles)) {
-      return true;
-    } else {
-      router.navigate(['/unauthorized']);
-      return false;
-    }
+  if (authService.hasRole(requiredRoles)) {
+    return true;
+  } else {
+    router.navigate(['/unauthorized']);
+    return false;
+  }
 }
 
 
